7b181487ecd36f83c17b967dd820588bdceb17ad
Farrux Madaminov +998937373322 <madaminovfarrux3322@gmail.com>
2023-07-18 10:50:31 +0500

    part1 2 1 3 4d 5 6

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,21 +1,65 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+import '../../data/network/api_provider.dart';
 
+
+class HomeScreen extends StatefulWidget {
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TranslationProvider _translationProvider = TranslationProvider();
+  String _translatedText = '';
+  TextEditingController _textEditingController = TextEditingController();
+
+  void _fetchTranslation(String query) async {
+    try {
+      _translatedText = await _translationProvider.getTranslation(query);
+      setState(() {});
+    } catch (e) {
+      setState(() {});
+    }
+  }
+
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: const Text("Default Project"),
-        centerTitle: true,
+        title: Text('Translation Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                labelText: 'Enter',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final query = _textEditingController.text.trim();
+                if (query.isNotEmpty) {
+                  _fetchTranslation(query);
+                }
+              },
+              child: Text('Translate'),
+            ),
+            SizedBox(height: 20),
+            Center(child: Text('Translated Text: $_translatedText')),
+          ],
+        ),
       ),
     );
   }
