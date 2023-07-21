0b3f096eecc4dee1e3c6e6a4bf93a55026d057bb
Farrux Madaminov +998937373322 <madaminovfarrux3322@gmail.com>
2023-07-21 16:56:43 +0500

    part1 2 1 3 4d 5 6 7 8

--- lib/data/models/place_holder_model.dart
+++ lib/data/models/place_holder_model.dart
@@ -1,1 +1,79 @@
-class PlaceHolderModel{}
+class AirLineModel {
+  final String id;
+  final int id1;
+  final String name;
+  final String country;
+  final String logo;
+  final String slogan;
+  final String head;
+  final String website;
+  final String established;
+
+  AirLineModel(
+      {required this.id,
+        required this.id1,
+        required this.name,
+        required this.country,
+        required this.logo,
+        required this.slogan,
+        required this.head,
+        required this.website,
+        required this.established});
+
+  factory AirLineModel.fromJson(Map<String, dynamic> json) {
+    return AirLineModel(
+        id: json['_id'],
+        id1: json['id'],
+        name: json['name'],
+        country: json['country'],
+        logo: json['logo'],
+        slogan: json['slogan'],
+        head: json['head_quaters'],
+        website: json['website'],
+        established: json['established']);
+  }
+}
+
+
+class DataModel {
+  final String id;
+  final String name;
+  final int trips;
+  final List<AirLineModel> airline;
+
+  DataModel(
+      {required this.id,
+        required this.name,
+        required this.trips,
+        required this.airline});
+
+  factory DataModel.fromJson(Map<String, dynamic> json) {
+    return DataModel(
+      id: json['id'],
+      name: json['name'],
+      trips: json['trips'],
+      airline: (json['airline'] as List<dynamic>?)
+          ?.map((e) => AirLineModel.fromJson(e as Map<String, dynamic>))
+          .toList() ??
+          [],);
+  }
+}
+
+
+class PassengerModel {
+  final int totalPassenger;
+  final int totalPages;
+  final DataModel data;
+
+  PassengerModel(
+      {required this.totalPassenger,
+        required this.totalPages,
+        required this.data});
+
+  factory PassengerModel.fromJson(Map<String, dynamic> json) {
+    return PassengerModel(
+        totalPassenger: json['totalPassengers'],
+        totalPages: json['totalPages'],
+        data: DataModel.fromJson(json['data']));
+  }
+}
---
+++ lib/data/network/api_provider.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+
+class ApiProvider{
+
+  static Future getAllData()async{
+    Uri uri = Uri.parse("https://api.instantwebtools.net/v1/passenger?page=2&size=10");
+
+    try{
+
+
+    }
+        catch(e){}
+  }
+
+}
